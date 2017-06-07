ff7f971fac921bc623212c8b99c50a59ba7bb346
Anton Adamansky <adamansky@gmail.com>
2017-06-08 02:13:16 +0700

    - Initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring boot Apache Cayenne starter
+
--- src/main/java/com/softmotions/cayenne/spring/conf/CayenneConfiguration.java
+++ src/main/java/com/softmotions/cayenne/spring/conf/CayenneConfiguration.java
@@ -11,9 +11,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -25,15 +29,19 @@ import com.softmotions.cayenne.utils.ExtBaseContext;
  * @author Adamansky Anton (adamansky@softmotions.com)
  */
 @SpringBootConfiguration
+@ComponentScan("com.softmotions.cayenne.spring")
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+@ConditionalOnClass({ServerRuntime.class})
 @ConditionalOnProperty(prefix = "spring.cayenne", name = "config")
-@ConditionalOnClass({DataSource.class})
 public class CayenneConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(CayenneConfiguration.class);
 
     @Bean
+    @ConditionalOnMissingBean
     public CayenneRuntime cayenneServerRuntime(DataSource dataSource,
                                                CayenneProperties props) {
+        log.info("Creating cayenne runtime, configuration: {}", props.getConfig());
         return ServerRuntime.builder()
                             .dataSource(dataSource)
                             .addConfig(props.getConfig())
@@ -44,10 +52,12 @@ public class CayenneConfiguration {
     @Primary
     public PlatformTransactionManager transactionManager(DataSource dataSource,
                                                          CayenneRuntime runtime) {
+        log.info("Creating CayenneTransactionManager instance");
         return new CayenneTransactionManager(runtime, dataSource);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public FactoryBean<ObjectContext> objectContext(CayenneRuntime cayenneRuntime) {
         return new FactoryBean<ObjectContext>() {
 
@@ -76,6 +86,10 @@ public class CayenneConfiguration {
 
     @Service
     static class CayenneServletRequestListener implements ServletRequestListener {
+
+        CayenneServletRequestListener() {
+            log.info("CayenneServletRequestListener instantiated");
+        }
 
         @Override
         public void requestInitialized(ServletRequestEvent sre) {
